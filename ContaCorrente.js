4da6c032c856bb5489aa593bbb68d7f6e9a3ef15
Isaac Spagnol <isaacmicael51@gmail.com>
2021-11-18 18:53:21 -0300

    set and get

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -17,6 +17,7 @@ export class ContaCorrente{
     get saldo(){
         this._saldo;
     }
+    
     // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0;
     
